a0e70195d0a2ad7bbfe953fcced7d1d7cbd3aa3d
dinhdam <dinhdam24492@gmail.com>
2018-11-27 10:16:02 +0700

    register login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , ReactiveFormsModule , FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
