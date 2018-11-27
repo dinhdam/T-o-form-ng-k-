d452c86bae339af9b22fe316e352d7461eb8ed17
dinhdam <dinhdam24492@gmail.com>
2018-11-27 16:38:37 +0700

    user register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+function  comparePassword (c: AbstractControl) {
+const v = c.value ;
+return (v.password === v.confirmPassword) ? null : {
+   passwordnotmatch : true
+  };
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.registerForm = this.fb.group({
+      email : ['', [Validators.required , Validators.email]],
+      pwGroup : this.fb.group({
+        password : '',
+        confirmPassword : ''
 
+      }, {vadilator : comparePassword})
+    });
+    this.registerForm.patchValue({
+    email: '@inforluutien@gmail.com'
+    });
+  }
+  onSubmit() {
+  }
 }
